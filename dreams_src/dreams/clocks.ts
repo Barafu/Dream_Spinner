b9e3779ce56bd592f6615691f4e82d1355ea5aee
Barafu Albino Cheetah <barafu_develops@albino.email>
2024-11-03 06:39:54 +0300

    Remove tauri

---
+++ dreams_src/dreams/clocks.ts
@@ -0,0 +1,106 @@
+// Clocks dream.
+
+export function clock(canvas: HTMLCanvasElement) {
+  const now = new Date();
+  const ctx = canvas.getContext("2d");
+  if (ctx === null) {
+    console.error("Failed to get 2D drawing context");
+    return;
+  }
+  ctx.save();
+  const canvas_dim = Math.min(canvas.width, canvas.height);
+  const canv_scale = canvas_dim / 330.0;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.translate(canvas.width / 2, canvas.height / 2);
+  ctx.scale(canv_scale, canv_scale);
+  ctx.rotate(-Math.PI / 2);
+  ctx.strokeStyle = "white";
+  ctx.fillStyle = "white";
+  ctx.lineWidth = 8;
+  ctx.lineCap = "round";
+
+  // Hour marks
+  ctx.save();
+  for (let i = 0; i < 12; i++) {
+    ctx.beginPath();
+    ctx.rotate(Math.PI / 6);
+    ctx.moveTo(100, 0);
+    ctx.lineTo(120, 0);
+    ctx.stroke();
+  }
+  ctx.restore();
+
+  // Minute marks
+  ctx.save();
+  ctx.lineWidth = 5;
+  for (let i = 0; i < 60; i++) {
+    if (i % 5 !== 0) {
+      ctx.beginPath();
+      ctx.moveTo(117, 0);
+      ctx.lineTo(120, 0);
+      ctx.stroke();
+    }
+    ctx.rotate(Math.PI / 30);
+  }
+  ctx.restore();
+
+  const sec = now.getSeconds() + now.getMilliseconds() / 1000;
+  const min = now.getMinutes();
+  const hr = now.getHours() % 12;
+
+  ctx.fillStyle = "white";
+
+  // Write image description
+  canvas.innerText = `The time is: ${hr}:${min}`;
+
+  // Write Hours
+  ctx.save();
+  ctx.rotate(
+    (Math.PI / 6) * hr + (Math.PI / 360) * min + (Math.PI / 21600) * sec,
+  );
+  ctx.lineWidth = 14;
+  ctx.beginPath();
+  ctx.moveTo(-20, 0);
+  ctx.lineTo(80, 0);
+  ctx.stroke();
+  ctx.restore();
+
+  // Write Minutes
+  ctx.save();
+  ctx.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec);
+  ctx.lineWidth = 10;
+  ctx.beginPath();
+  ctx.moveTo(-28, 0);
+  ctx.lineTo(112, 0);
+  ctx.stroke();
+  ctx.restore();
+
+  // Write seconds
+  ctx.save();
+  ctx.rotate((sec * Math.PI) / 30);
+  ctx.strokeStyle = "#D40000";
+  ctx.fillStyle = "#D40000";
+  ctx.lineWidth = 6;
+  ctx.beginPath();
+  ctx.moveTo(-30, 0);
+  ctx.lineTo(83, 0);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(0, 0, 10, 0, Math.PI * 2, true);
+  ctx.fill();
+  ctx.beginPath();
+  ctx.arc(95, 0, 10, 0, Math.PI * 2, true);
+  ctx.stroke();
+  ctx.fillStyle = "rgb(0 0 0 / 0%)";
+  ctx.arc(0, 0, 3, 0, Math.PI * 2, true);
+  ctx.fill();
+  ctx.restore();
+
+  ctx.beginPath();
+  ctx.lineWidth = 14;
+  ctx.strokeStyle = "#325FA2";
+  ctx.arc(0, 0, 142, 0, Math.PI * 2, true);
+  ctx.stroke();
+
+  ctx.restore();
+}
---
+++ dreams_src/main.ts
@@ -0,0 +1,43 @@
+import { exit } from "@tauri-apps/plugin-process";
+//import { getCurrentWebviewWindow } from '@tauri-apps/api/webviewWindow';
+import { getCurrentWindow } from "@tauri-apps/api/window";
+
+import { clock } from "./dreams/clocks";
+import { dendraClock } from "./dreams/dendraclock";
+
+let canvas: HTMLCanvasElement = document.getElementById(
+  "dreamCanvas",
+) as HTMLCanvasElement;
+
+let dream: string = "dendraclock";
+
+addEventListener("mouseup", (event) => {
+  if (event.button == 0) {
+    exit(0);
+  }
+});
+
+window.addEventListener("resize", resizeCanvas);
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+function animate() {
+  if (dream == "clock") {
+    clock(canvas);
+  }
+  if (dream == "dendraclock") {
+    dendraClock(canvas);
+  }
+  window.requestAnimationFrame(animate);
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  resizeCanvas();
+  animate();
+
+  const appWindow = getCurrentWindow();
+  appWindow.show();
+});
