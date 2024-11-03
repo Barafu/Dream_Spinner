a5871d4fef7f0c445da85572fc6fecb467bdb2ad
Barafu Albino Cheetah <barafu_develops@albino.email>
2024-11-03 10:48:58 +0300

    Display windows after they were rendered

--- dreams_src/main.ts
+++ dreams_src/main.ts
@@ -4,38 +4,24 @@ import { dendraClock } from "./dreams/dendraclock";
 
 declare var pywebview: any;
 
-let pywebviewready = false;
-let DOMReady = false;
 let canvas: HTMLCanvasElement | null = null;
 let dream = "dendraclock";
 
 window.addEventListener('pywebviewready', function () {
-  pywebviewready = true;
-  big_init();
-})
-
-window.addEventListener("DOMContentLoaded", function () {
-  DOMReady = true;
-  big_init();
-});
-
-function big_init() {
-  if (!pywebviewready || !DOMReady) {
-    return;
-  }
   window.addEventListener("mouseup", (event) => {
     if (event.button == 0) {
       pywebview.api.quit();
     }
   });
+})
+
+window.addEventListener("DOMContentLoaded", function () {
   window.addEventListener("resize", resizeCanvas);
   canvas = document.getElementById("dreamCanvas") as HTMLCanvasElement;
   let dream: string = "dendraclock";
   resizeCanvas();
   animate();
-  
-}
-
+});
 
 function resizeCanvas() {
   if (canvas) {
--- python_src/dreamspinner.py
+++ python_src/dreamspinner.py
@@ -2,18 +2,27 @@ import webview
 import sys
 
 # Some control variables
-PACK_MODE = "DEV"
-# PACK_MODE = "RELEASE"
+ALLOW_QUIT = True  # Blocks quitting on mouse, for debug purposes
 
-ALLOW_QUIT = True
 
-if PACK_MODE == "RELEASE":
-    HOST_ADDR = "dist/index.html"
-else:
-    HOST_ADDR = "http://localhost:5173/"
+def is_release() -> bool:
+    """Returns whether the app is running in release mode (aka inside PyInstaller)"""
+    return getattr(sys, "frozen", False) and hasattr(sys, "_MEIPASS")
+
+
+def determine_host_addr() -> str:
+    """Returns the address to connnect the browser to, depending on whether the application is running from the bundle, or not."""
+    if is_release():
+        # in pyinstaller
+        return "dist/index.html"
+    else:
+        # in development
+        return "http://localhost:5173/"
 
 
 class Connection:
+    """Bundles functions to expose to JS side"""
+
     def __init__(self, app: "App"):
         self.app = app
 
@@ -25,26 +34,32 @@ class Connection:
             self.app.stop()
 
 
+def show_window(window):
+    window.show()
+
+
 class App:
     def __init__(self):
         self.windows: list[webview.Window] = []
         self.connection = Connection(self)
 
     def create_windows(self):
-        for screen in webview.screens:
+        """Create one fullscreen window per monitor. Windows are initially hidden to avoid showing them before they are ready"""
+        host_addr = determine_host_addr()
+        for i, screen in enumerate(webview.screens):
             w = webview.create_window(
-                "Test",
-                HOST_ADDR,
+                f"DreamSpinner {i}",
+                host_addr,
                 screen=screen,
-                resizable=True,
-                width=1000,
-                height=800,
                 fullscreen=True,
                 js_api=self.connection,
+                hidden=True,
             )
+            w.events.loaded += show_window
             self.windows.append(w)
 
     def stop(self):
+        """Stops the app, closing all its windows."""
         for w in self.windows:
             w.destroy()
 
@@ -53,4 +68,5 @@ if __name__ == "__main__":
     app = App()
     app.create_windows()
     webview.settings["OPEN_DEVTOOLS_IN_DEBUG"] = False
-    webview.start(debug=True)
+    allow_debug = not is_release()
+    webview.start(debug=allow_debug)
